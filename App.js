21a1195cf344a44f8d28ae45453124442b478d80
LeoAlain <leanalain@gmail.com>
2022-04-04 11:37:07 -0400

    created app

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+  useDrawerStatus,
+} from '@react-navigation/drawer';
+import Animated, { Extrapolate } from 'react-native-reanimated';
 
-export default function App() {
+function Feed() {
+  const isOpenDrawer = useDrawerStatus();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Feed Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function Article() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Article Screen</Text>
+    </View>
+  );
+}
+
+function CustomDrawerContent({ progress, ...rest }) {
+  const translateX = Animated.interpolateNode(progress, {
+    inputRange: [0, 0.5, 1],
+    outputRange: [1, 0.9, 0.85],
+    extrapolateRight: Extrapolate.CLAMP
+  });
+
+  const borderRadius = Animated.interpolateNode(progress, {
+    inputRange: [0, 1],
+    outputRange: [1, 20],
+    extrapolateRight: Extrapolate.CLAMP
+  });
+
+  return (
+    <DrawerContentScrollView {...rest}>
+      <Animated.View style={{ transform: [{ translateX }], borderRadius }}>
+        <DrawerItemList {...rest} />
+        <DrawerItem label="Help" onPress={() => alert('Link to help')} />
+      </Animated.View>
+    </DrawerContentScrollView>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyDrawer />
+    </NavigationContainer>
+  );
+}
